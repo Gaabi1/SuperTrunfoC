afc37acf813924cae2b7022d0913edf7fcd234ed
Gabi <143514875+Gaabi1@users.noreply.github.com>
2025-06-03 15:54:49 +0000

    novo commit

--- BatalhaNaval/desafio.c
+++ BatalhaNaval/desafio.c
@@ -1,31 +1,69 @@
 #include <stdio.h>
 
-int main(){
+#define TAM 10
+#define NAVIOS 4
 
-    char linha [10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+char cabecalho [10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     
-    int matriz [10][10];
+int matriz [TAM][TAM];
 
-    // Inicializa toda a matriz com 0
-    for (int i = 0; i<10; i++) {
-        for (int j= 0; j<10; j++){
-            matriz[i][j]= 0;
+// Estrutura para armazenar um navio
+typedef struct {
+    int x; //coluna
+    int y; //linha
+    int tamanho;
+    char direcao; // 'H', 'V' ou 'D' (diagonal)
+}Navio;
+
+// Verifica se a posição é válida e não sobrepõe
+int pode_posicionar (Navio n){
+    for (int i =0; i< n.tamanho; i++){
+        int linha= n.y;
+        int coluna= n.x;
+        if (n.direcao== 'H') coluna += i;
+        else if (n.direcao == 'V') linha += i;
+        else if (n.direcao == 'D'){
+            linha += i;
+            coluna += i;
         }
+
+        // Verifica limites
+        if (linha >= TAM || coluna >= TAM)
+            return 0;
+
+        // Verifica sobreposição
+        if (matriz[linha][coluna] != 0)
+            return 0;
     }
+        return 1;
+}
 
-    // Altera valores específicos na linha 2 (índice 2), colunas 2, 3 e 4
-    matriz[2][2] = 3;
-    matriz[2][3] = 3;
-    matriz[2][4] = 3;
+// Posiciona o navio no tabuleiro
+void posicionar_navio(Navio n) {
+    for (int i = 0; i < n.tamanho; i++) {
+        int linha = n.y;
+        int coluna = n.x;
 
-    printf("   ");
+        if (n.direcao == 'H') coluna += i;
+        else if (n.direcao == 'V') linha += i;
+        else if (n.direcao == 'D') {
+            linha += i;
+            coluna += i;
+        }
+
+        matriz[linha][coluna] = 3;
+    }
+}
+
+void imprimir_tabuleiro(){
+    printf("  ");
     for ( int g=0 ; g<10; g++){
-        printf(" %c",linha[g]);
+        printf(" %c",cabecalho[g]);
 
     }
     printf("\n");
 
-    for (int i= 1; i<11; i++){
+    for (int i= 0; i<10; i++){
         printf("%2d", i);  // Índice da linha (1 a 10)
         for(int j= 0; j<10; j++){
             printf(" %d", matriz[i][j]);
@@ -33,6 +71,29 @@ int main(){
         printf("\n");
     }
 
-    return 0;
+}
+int main() {
+    // Inicializa tabuleiro com 0
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            matriz[i][j] = 0;
 
+    Navio navios[NAVIOS] = {
+        {0, 0, 4, 'H'}, // horizontal
+        {2, 2, 3, 'V'}, // vertical
+        {4, 4, 3, 'D'}, // diagonal
+        {7, 1, 2, 'D'}  // diagonal
+    };
+
+    for (int i = 0; i < NAVIOS; i++) {
+        if (pode_posicionar(navios[i])) {
+            posicionar_navio(navios[i]);
+        } else {
+            printf("Erro: navio %d comeca em (%d,%d) e não pode ser posicionado.\n", i + 1, navios[i].x, navios[i].y);
+        }
+    }
+
+    imprimir_tabuleiro();
+
+    return 0;
 }
